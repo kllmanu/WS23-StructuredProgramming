ee35967b86612dabeaa7364bdc90b2092a1d0de4
Manuel Koell <man.koell@gmail.com>
2023-12-13 17:29:44 +0100

    print function header once

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 import os
+from pprint import pprint
 
 width, height = os.get_terminal_size()
 divider = '-' * width
+tested = []
 
 def test(fn, *args, **kwargs):
-    print(divider)
-
-    if fn.__doc__:
+    if fn.__doc__ and fn.__name__ not in tested:
+        print(divider)
         print(fn.__doc__.split('\n')[0])
-        print()
+
+    print()
 
     result = fn(*args, **kwargs)
 
     if result is not None:
-        print('  ', result)
+        pprint(result)
+
+    tested.append(fn.__name__)
